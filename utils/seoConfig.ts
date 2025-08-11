a50e3ebdcdf62971f0303a4181a1534c6dac7240
ismaelazanza <ismaelazanza@gmail.com>
2025-08-10 21:28:51 -0500

    1.1

--- utils/seoConfig.ts
+++ utils/seoConfig.ts
@@ -5,7 +5,7 @@ import type { ManifestOptions } from "vite-plugin-pwa"
  * Defines the default SEO configuration for the website.
  */
 export const seoConfig = {
-	baseURL: "https://example.com", // Change this to your production URL.
+	baseURL: "https://radiolatidos893.com", // Change this to your production URL.
 	description:
 		"Astro PWA Starter is an opionated Astro starter for building robust static websites.", // Change this to be your website's description.
 	type: "website",
@@ -15,7 +15,7 @@ export const seoConfig = {
 		width: 1200,
 		height: 630
 	},
-	siteName: "Astro PWA Starter", // Change this to your website's name,
+	siteName: "Radio Latidos 89.3", // Change this to your website's name,
 	twitter: {
 		card: "summary_large_image"
 	}
@@ -25,8 +25,8 @@ export const seoConfig = {
  * Defines the configuration for PWA webmanifest.
  */
 export const manifest: Partial<ManifestOptions> = {
-	name: "Astro PWA Starter", // Change this to your website's name.
-	short_name: "Astro PWA Starter", // Change this to your website's short name.
+	name: "Radio Latidos 89.3", // Change this to your website's name.
+	short_name: "Radio Latidos 89.3", // Change this to your website's short name.
 	description:
 		"Astro PWA Starter is an opionated Astro starter for building robust static websites.", // Change this to your websites description.
 	theme_color: "#30E130", // Change this to your primary color.
